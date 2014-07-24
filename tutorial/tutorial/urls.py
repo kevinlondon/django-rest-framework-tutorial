4d730da345fa0cc609fc384d04f11656eb7ce558
kevinlondon <kevinlondon@gmail.com>
2014-07-23 22:35:19 -0700

    End of the first tutorial but still need to do a little debugging.

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -4,8 +4,5 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('snippets.urls'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('snippets.urls')),
 )
